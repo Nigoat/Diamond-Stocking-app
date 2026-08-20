3b7827824a28409ecbcfa0ecf431b949d7e0b332
Nigoat <nizardev123@gmail.com>
2026-08-20 21:02:08 +0100

    Initial commit of Diamond Inventory source files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nigoat/Documents/Diamond
-# Build directory: /home/nigoat/Documents/Diamond/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
